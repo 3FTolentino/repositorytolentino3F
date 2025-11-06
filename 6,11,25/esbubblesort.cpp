8f50c7adedf738338014509a7ad47308f456e58a
Cirulli <infantino@gmail.com>
2025-11-06 09:47:10 +0100

    "esercizio bubble sort"

---
+++ 6,11,25/esbubblesort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n, cont;
+    cout << "Inserisci il numero di elementi da ordinare: ";
+    cin >> n ;
+    int arr[n];
+   
+   for (cont=0 ; cont<n ; cont++) {
+   cout << "Inserisci gli elementi da ordinare: " << endl;
+   cin >> arr[cont];
+
+
+
+   }
+    
+   return 0;
+}
+
+
