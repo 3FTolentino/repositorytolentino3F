12d141fb9fa45e3f94609d510ffac65b3033d9fb
3FTolentino <franz31roland@gmail.com>
2025-12-01 22:40:47 +0100

    compiti per il 2 Dicembre

---
+++ 01-12-2025_comp_tolentino_franz/pg117/es1.c
@@ -0,0 +1,21 @@
+// Determina, tramite un opportuno algoritmo, se un dato numero è positivo o negativo.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    float num;
+    printf("Inserisci un numero: ");
+    scanf("%f" , &num);
+
+    if (num > 0) {
+        printf("Il numero e' positivo");
+    }
+    else {
+        printf("Il numero e' negativo");
+    }
+
+    return 0;
+}
