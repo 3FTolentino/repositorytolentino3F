c3146babdc46a50d7a4b74b8a3d8ccf2746a6c6b
3FTolentino <franz31roland@gmail.com>
2025-12-02 18:58:24 +0100

    compiti x 3 Dicembre

--- 02-12-25_comp_tolentino_franz/pg117/es6.c
+++ 02-12-25_comp_tolentino_franz/pg117/es6.c
@@ -3,6 +3,20 @@
 
 int main () {
 
+    char lett;
+
+    printf("Digita un carattere (S/s/N/n): ");
+    scanf(" %c", &lett);   
+
+    if (lett == 'S' || lett == 's') {
+        printf("Si\n");
+    }
+    else if (lett == 'N' || lett == 'n') {
+        printf("No\n");
+    }
+    else {
+        printf("Errato\n");
+    }
     
     return 0;
 }
